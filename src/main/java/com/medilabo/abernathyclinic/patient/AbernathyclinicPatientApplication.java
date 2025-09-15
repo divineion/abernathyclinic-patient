7eb211844cf681454252eeba596969f36168423e
divineion <najima.guermoudi@gmail.com>
2025-09-15 17:27:38 +0200

    feat : initial Spring boot project setup
    Initialize Spring Boot project with Web, JPA and Validation starters.
    Add Postgresql for prod database, and H2 dependencies for testing.

---
+++ src/main/java/com/medilabo/abernathyclinic/patient/AbernathyclinicPatientApplication.java
@@ -0,0 +1,13 @@
+package com.medilabo.abernathyclinic.patient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbernathyclinicPatientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbernathyclinicPatientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medilabo/abernathyclinic/patient/AbernathyclinicPatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.abernathyclinic.patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbernathyclinicPatientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
