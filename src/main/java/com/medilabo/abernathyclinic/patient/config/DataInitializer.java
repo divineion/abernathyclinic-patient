e0cbe5670d8d782a58082862de0fabf1fddc656f
divineion <najima.guermoudi@gmail.com>
2025-09-24 17:44:04 +0200

    feat(data): insert data into table street
    Add method `initStreets()̀ :
     - create a list of 4 new streets, each one related to an existing city,
     - iterate over the list to check if each street already exists,
    otherwise save it in the DB.
    
    Call `initStreets()` in `insertDevData()`.

--- src/main/java/com/medilabo/abernathyclinic/patient/config/DataInitializer.java
+++ src/main/java/com/medilabo/abernathyclinic/patient/config/DataInitializer.java
@@ -1,7 +1,9 @@
 package com.medilabo.abernathyclinic.patient.config;
 
 import com.medilabo.abernathyclinic.patient.entity.City;
+import com.medilabo.abernathyclinic.patient.entity.Street;
 import com.medilabo.abernathyclinic.patient.repository.CityRepository;
+import com.medilabo.abernathyclinic.patient.repository.StreetRepository;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,15 +18,18 @@ import org.springframework.context.annotation.Profile;
 public class DataInitializer {
 
 	private final CityRepository cityRepository;
-
-	DataInitializer(CityRepository cityRepository) {
+	private final StreetRepository streetRepository;
+	
+	DataInitializer(CityRepository cityRepository, StreetRepository streetRepository) {
 		this.cityRepository = cityRepository;
+		this.streetRepository = streetRepository;
 	}
 
 	@Bean
 	CommandLineRunner insertDevData() {
 		return _ -> {
 			initCities();
+			initStreets();
 		};
 	}
 
@@ -46,6 +51,32 @@ public class DataInitializer {
 		newCities.forEach(city -> {
 			if (cityRepository.findByNameAndZip(city.getName(), city.getZip()).isEmpty()) {
 				cityRepository.save(city);
+			}
+		});
+	}
+		
+	private void initStreets() {
+		List<Street> streets = new ArrayList<>();
+	
+		City city1 = cityRepository.findByNameAndZip("Cambridge", "10139").get();
+		Street street1 = new Street("Brookside St", city1);
+		streets.add(street1);
+	
+		City city2 = cityRepository.findByNameAndZip("North Platte", "20139").get();
+		Street street2 = new Street("High St", city2);
+		streets.add(street2);
+	
+		City city3 = cityRepository.findByNameAndZip("San Diego", "30139").get();
+		Street street3 = new Street("Club Road", city3);
+		streets.add(street3);
+	
+		City city4 = cityRepository.findByNameAndZip("Bethia", "40139").get();
+		Street street4 = new Street("Valley Dr", city4);
+		streets.add(street4);
+	
+		streets.forEach(street -> {
+			if (streetRepository.findByNameAndCity(street.getName(), street.getCity()).isEmpty()) {
+				streetRepository.save(street);
 			}
 		});
 	}
