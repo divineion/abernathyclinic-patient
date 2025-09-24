28f670aeb4795b83a56f5e8702c13af188ccee62
divineion <najima.guermoudi@gmail.com>
2025-09-24 18:27:43 +0200

    feat(repo): add `findByNumberAndStreet()` method in AddressRepository
    Add custom JPQL query method to retrieve a unique address by street
    number and Street.

--- src/main/java/com/medilabo/abernathyclinic/patient/repository/AddressRepository.java
+++ src/main/java/com/medilabo/abernathyclinic/patient/repository/AddressRepository.java
@@ -1,9 +1,14 @@
 package com.medilabo.abernathyclinic.patient.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.medilabo.abernathyclinic.patient.entity.Address;
+import com.medilabo.abernathyclinic.patient.entity.Street;
 
 public interface AddressRepository extends JpaRepository<Address, Integer> {
-
+	@Query("select a from Address a where a.streetNumber = :streetNumber and a.street = :street")
+	public Optional<Address> findByNumberAndStreet(String streetNumber, Street street);
 }
