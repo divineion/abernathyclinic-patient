ba3d2f1652559fddb40fc70ffa776a0e28f14a15
divineion <najima.guermoudi@gmail.com>
2025-09-26 12:18:13 +0200

    refactor(repository): override findById() with join fetch
    Override default `findById()` to load Patient with address, street and
    city in a single request.

--- src/main/java/com/medilabo/abernathyclinic/patient/repository/PatientRepository.java
+++ src/main/java/com/medilabo/abernathyclinic/patient/repository/PatientRepository.java
@@ -2,6 +2,7 @@ package com.medilabo.abernathyclinic.patient.repository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,4 +13,8 @@ public interface PatientRepository extends JpaRepository<Patient, Long> {
 	
 	@Query("select p from Patient p where p.lastName = :lastName and p.firstName = :firstName and p.birthDate = :birthDate")
 	List<Patient> findByNameAndBirthDate(String lastName, String firstName, LocalDate birthDate);
+	
+	@Override
+	@Query("select p from Patient p join fetch p.address join fetch p.address.street join fetch p.address.street.city where p.id =:id")
+	Optional<Patient> findById(Long id);
 }
