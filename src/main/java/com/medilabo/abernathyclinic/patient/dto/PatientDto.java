f1cf3fbaa4305a6e3d0ae7403c2eb8a4aefda275
divineion <najima.guermoudi@gmail.com>
2025-09-27 00:17:07 +0200

    feat(patient-dto): add phone property

--- src/main/java/com/medilabo/abernathyclinic/patient/dto/PatientDto.java
+++ src/main/java/com/medilabo/abernathyclinic/patient/dto/PatientDto.java
@@ -3,4 +3,4 @@ package com.medilabo.abernathyclinic.patient.dto;
 import java.time.LocalDate;
 import java.util.UUID;
 
-public record PatientDto(UUID uuid, String lastName, String firstName, LocalDate birthDate, String gender, AddressDto address) {}
+public record PatientDto(UUID uuid, String lastName, String firstName, LocalDate birthDate, String gender, AddressDto address, String phone) {}
