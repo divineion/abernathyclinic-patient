ffaf4b7e837cfa2e6587cd5014fb148f0fca28e6
divineion <najima.guermoudi@gmail.com>
2025-09-27 04:14:58 +0200

    feat(mapper): add `createPatientDtoToPatient()`
    Map CreatePatientDto -> Patient.
    Ignore `address` field (handled in PatientService)

--- src/main/java/com/medilabo/abernathyclinic/patient/service/patient/PatientMapper.java
+++ src/main/java/com/medilabo/abernathyclinic/patient/service/patient/PatientMapper.java
@@ -3,7 +3,9 @@ package com.medilabo.abernathyclinic.patient.service.patient;
 import java.util.List;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+import com.medilabo.abernathyclinic.patient.dto.CreatePatientDto;
 import com.medilabo.abernathyclinic.patient.dto.MinimalPatientDto;
 import com.medilabo.abernathyclinic.patient.dto.PatientDto;
 import com.medilabo.abernathyclinic.patient.entity.Patient;
@@ -15,4 +17,8 @@ public interface PatientMapper {
 	public PatientDto patientToPatientDto(Patient patient);
 
 	public List<MinimalPatientDto> patientsListToMinimalPatientDtoList(List<Patient> patientsList);
+	
+	@Mapping(target = "address", ignore = true)
+    Patient createPatientDtoToPatient(CreatePatientDto dto);
+
 }
