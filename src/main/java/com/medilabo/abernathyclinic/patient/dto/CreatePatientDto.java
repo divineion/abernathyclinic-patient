0b15d9a857829a2e80655017913d9b9cabd53c06
divineion <najima.guermoudi@gmail.com>
2025-09-28 23:18:36 +0200

    feat(dto): add a custom constructor in CreatePatientDto
    Add a custom constructor to accept null values for address and phone
    (allowing JSON deserialisation when these fields are absent)

--- src/main/java/com/medilabo/abernathyclinic/patient/dto/CreatePatientDto.java
+++ src/main/java/com/medilabo/abernathyclinic/patient/dto/CreatePatientDto.java
@@ -3,5 +3,7 @@ package com.medilabo.abernathyclinic.patient.dto;
 import java.time.LocalDate;
 
 public record CreatePatientDto(String firstName, String lastName, LocalDate birthDate, String gender, AddressDto address, String phone) {
-
+	public CreatePatientDto(String firstName, String lastName, LocalDate birthDate, String gender) {
+		this(firstName, lastName, birthDate, gender, null, null);
+	}
 }
