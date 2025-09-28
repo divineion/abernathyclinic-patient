6e32065f8c06e0718dd9f40e8137387f6fee0c8c
divineion <najima.guermoudi@gmail.com>
2025-09-28 23:36:49 +0200

    test(patient): add integration tests for patient creation
    Add tests covering :
     - creation with all fields,
     - creation without address,
     - creation without phone
    
    Verify JSON contains values for all require fields and null for optional
    fields when absent.

--- src/test/java/com/medilabo/abernathyclinic/patient/it/PatientControllerIT.java
+++ src/test/java/com/medilabo/abernathyclinic/patient/it/PatientControllerIT.java
@@ -1,15 +1,24 @@
 package com.medilabo.abernathyclinic.patient.it;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.time.LocalDate;
+
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.medilabo.abernathyclinic.patient.dto.AddressDto;
+import com.medilabo.abernathyclinic.patient.dto.CreatePatientDto;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -17,6 +26,9 @@ import org.springframework.test.web.servlet.MockMvc;
 public class PatientControllerIT {
 	@Autowired
 	MockMvc mockMvc;
+	
+	@Autowired
+	ObjectMapper objectMapper;
 
 	@Test
 	public void testGetPatientById_shouldReturnPatientDto() throws Exception {
@@ -59,5 +71,69 @@ public class PatientControllerIT {
 			jsonPath("$.[0].birthDate").isString(),
 			jsonPath("$.[0].gender").isString()
 		);
+	}
+	
+	@Test
+	public void testCreatePatient_shouldReturnCreated() throws Exception {
+		AddressDto addressDto = new AddressDto("1", "Dream str", "Sringfield", "45890");
+		CreatePatientDto dto = new CreatePatientDto("Testfirstname", "Testlastname", LocalDate.of(2001, 10, 01), "f", addressDto, "004-901-019");
+		
+		mockMvc.perform(post("/api/patient")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(dto)))
+		.andExpectAll(
+				status().isCreated(),
+				jsonPath("$.uuid").isString(),
+				jsonPath("$.firstName").isString(),
+				jsonPath("$.lastName").isString(),
+				jsonPath("$.birthDate").isString(),
+				jsonPath("$.gender").isString(),
+				jsonPath("$.address.streetNumber").isString(),
+				jsonPath("$.address.street").isString(),
+				jsonPath("$.address.city").isString(),
+				jsonPath("$.address.zip").isString(),
+				jsonPath("$.phone").isString()
+			);
+	}
+	
+	@Test
+	public void testCreatePatient_withoutAddress_shouldReturnCreated() throws Exception {
+		CreatePatientDto dto = new CreatePatientDto("Testfirstname", "Testlastname", LocalDate.of(2001, 10, 01), "f", null, "004-901-019");
+	
+		mockMvc.perform(post("/api/patient")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(dto)))
+		.andExpectAll(
+				status().isCreated(),
+				jsonPath("$.uuid").isString(),
+				jsonPath("$.firstName").isString(),
+				jsonPath("$.lastName").isString(),
+				jsonPath("$.gender").isString(),
+				jsonPath("$.birthDate").isString(),
+				jsonPath("$.address").value(Matchers.nullValue()),
+				jsonPath("$.phone").isString()
+		);
+	}
+
+	@Test
+	public void testCreatePatient_withoutPhone_shouldReturnCreated() throws Exception {
+		AddressDto addressDto = new AddressDto("1", "Dream str", "Sringfield", "45890");
+		CreatePatientDto dto = new CreatePatientDto("Testfirstname", "Testlastname", LocalDate.of(2001, 10, 01), "f", addressDto, null);
+		
+		mockMvc.perform(post("/api/patient")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(objectMapper.writeValueAsString(dto)))
+		.andExpectAll(
+				status().isCreated(),
+				jsonPath("$.uuid").isString(),
+				jsonPath("$.firstName").isString(),
+				jsonPath("$.lastName").isString(),
+				jsonPath("$.gender").isString(),
+				jsonPath("$.address.streetNumber").isString(),
+				jsonPath("$.address.street").isString(),
+				jsonPath("$.address.city").isString(),
+				jsonPath("$.address.zip").isString(),
+				jsonPath("$.phone").value(Matchers.nullValue())
+			);
 	}
 }
