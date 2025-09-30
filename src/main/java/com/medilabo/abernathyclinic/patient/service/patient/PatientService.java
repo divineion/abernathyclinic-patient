a51cb97ec6d167cddf2c5fda6dbbf73ac59b787f
divineion <najima.guermoudi@gmail.com>
2025-09-30 11:40:02 +0200

    feat(service): add method to update Patient
    Add `updatePatient(UpdatePatientDto dto)`:
     - check each property in the DTO for changes,
     - update the Patient entity with the appropriate setter when changed,
     - return PatientDto.
    
     Add `@Transactional` on method to prevent partial updates.

--- src/main/java/com/medilabo/abernathyclinic/patient/service/patient/PatientService.java
+++ src/main/java/com/medilabo/abernathyclinic/patient/service/patient/PatientService.java
@@ -4,15 +4,19 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.medilabo.abernathyclinic.patient.constants.ApiMessages;
 import com.medilabo.abernathyclinic.patient.dto.AddressDto;
 import com.medilabo.abernathyclinic.patient.dto.CreatePatientDto;
 import com.medilabo.abernathyclinic.patient.dto.MinimalPatientDto;
 import com.medilabo.abernathyclinic.patient.dto.PatientDto;
+import com.medilabo.abernathyclinic.patient.dto.UpdatePatientDto;
 import com.medilabo.abernathyclinic.patient.entity.Address;
 import com.medilabo.abernathyclinic.patient.entity.Patient;
 import com.medilabo.abernathyclinic.patient.exception.PatientNotFoundException;
 import com.medilabo.abernathyclinic.patient.repository.PatientRepository;
 import com.medilabo.abernathyclinic.patient.service.address.AddressService;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class PatientService {
@@ -55,5 +59,37 @@ public class PatientService {
 	
 	public Address addAddressIfNotExists(AddressDto address) {
 		return addressService.addAddress(address);
+	}
+	
+	@Transactional(rollbackOn = Exception.class)
+	public PatientDto updatePatient(Long id, UpdatePatientDto dto) throws PatientNotFoundException {
+		Patient patient = patientRepository.findById(id)
+				.orElseThrow(() -> new PatientNotFoundException(ApiMessages.PATIENT_NOT_FOUND + id));
+		
+		if (!patient.getLastName().equalsIgnoreCase(dto.lastName())) {
+			patient.setLastName(dto.lastName());
+		}
+		
+		if (!patient.getFirstName().equalsIgnoreCase(dto.firstName())) {
+			patient.setFirstName(dto.firstName());
+		}
+		
+		if (!patient.getGender().equalsIgnoreCase(dto.gender())) {
+			patient.setGender(dto.gender());
+		}
+		
+		if (dto.address() != null) {
+			Address providedAddress = addAddressIfNotExists(dto.address());
+			
+			if (!patient.getAddress().equals(providedAddress)) {
+				patient.setAddress(providedAddress);
+			}
+		}
+		
+		if (!patient.getPhone().equalsIgnoreCase(dto.phone())) {
+			patient.setPhone(dto.phone());
+		}
+
+		return patientMapper.patientToPatientDto(patient);
 	}
 }
