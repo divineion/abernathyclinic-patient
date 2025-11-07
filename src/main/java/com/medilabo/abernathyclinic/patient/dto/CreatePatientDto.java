a201762936e287b00e5039090f4949a1b34ee099
divineion <najima.guermoudi@gmail.com>
2025-11-07 01:50:23 +0100

    feat(validation): add validation constraints for nullable fields in DTO
    Annotate `phone` field with `@Size(min=0, max=20)` to allow empty field
    while controlling max character limit.

--- src/main/java/com/medilabo/abernathyclinic/patient/dto/CreatePatientDto.java
+++ src/main/java/com/medilabo/abernathyclinic/patient/dto/CreatePatientDto.java
@@ -2,6 +2,7 @@ package com.medilabo.abernathyclinic.patient.dto;
 
 import java.time.LocalDate;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -14,8 +15,8 @@ public record CreatePatientDto(
 		@NotBlank @Size(min=2, max=80) String lastName, 
 		@NotNull @JsonFormat(pattern = "yyyy-MM-dd") LocalDate birthDate, 
 		@NotBlank @Size(min=1, max=10) String gender, 
-		AddressDto address, 
-		String phone) {
+		@Valid AddressDto address,
+		@Size(min=0, max=20) String phone) {
 	public CreatePatientDto(String firstName, String lastName, LocalDate birthDate, String gender) {
 		this(firstName, lastName, birthDate, gender, null, null);
 	}
--- src/main/java/com/medilabo/abernathyclinic/patient/dto/UpdatePatientDto.java
+++ src/main/java/com/medilabo/abernathyclinic/patient/dto/UpdatePatientDto.java
@@ -9,8 +9,8 @@ public record UpdatePatientDto(
 		@NotBlank @Size(min=2, max=80) String firstName, 
 		@NotBlank @Size(min=2, max=80) String lastName, 
 		@NotBlank @Size(min=1, max=10) String gender, 
-		@Valid AddressDto address, 
-		String phone) {
+		@Valid AddressDto address,
+		@Size(min=0, max=20) String phone) {
 	public UpdatePatientDto(String firstName, String lastName, String gender) {
 		this(firstName, lastName, gender, null, null);
 	}
