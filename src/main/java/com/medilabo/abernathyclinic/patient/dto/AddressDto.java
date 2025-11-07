c2ba916ffc67a81a6ddd201890903ccec61c070b
divineion <najima.guermoudi@gmail.com>
2025-11-07 01:16:27 +0100

    feat(validation): accept blank address fields (for empty address)
    Accept strings from 0 char for AddressDto fields : this allow to accept
    either empty addresses, or complete addresses.
    Empty fields and partials addresses are handled in PatientService.

--- src/main/java/com/medilabo/abernathyclinic/patient/dto/AddressDto.java
+++ src/main/java/com/medilabo/abernathyclinic/patient/dto/AddressDto.java
@@ -3,7 +3,7 @@ package com.medilabo.abernathyclinic.patient.dto;
 import jakarta.validation.constraints.Size;
 
 public record AddressDto(
-		@Size(min = 1, max=5) String streetNumber, 
-		@Size(min = 2, max=50) String street, 
-		@Size(min = 2, max=50) String city,
-		@Size(min = 2, max=5) String zip) {}
+		@Size(min = 0, max=5) String streetNumber, 
+		@Size(min = 0, max=50) String street, 
+		@Size(min = 0, max=50) String city,
+		@Size(min = 0, max=5) String zip) {}
