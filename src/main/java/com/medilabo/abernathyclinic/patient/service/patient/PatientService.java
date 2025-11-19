54c9d3649f80165e95ea9b4db73cab59d67691c1
divineion <najima.guermoudi@gmail.com>
2025-11-19 10:21:40 +0100

    chore(dal): sort patients by last name in findAll()

--- src/main/java/com/medilabo/abernathyclinic/patient/service/patient/PatientService.java
+++ src/main/java/com/medilabo/abernathyclinic/patient/service/patient/PatientService.java
@@ -3,6 +3,8 @@ package com.medilabo.abernathyclinic.patient.service.patient;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.medilabo.abernathyclinic.patient.constants.ApiMessages;
@@ -96,7 +98,9 @@ public class PatientService {
 	 * @return
 	 */
 	public List<MinimalPatientDto> findAllPatient() {
-		List<Patient> patientsList = patientRepository.findAll();
+		Sort sort = Sort.by(Direction.ASC, "lastName");
+
+		List<Patient> patientsList = patientRepository.findAll(sort);
 		
 		List<MinimalPatientDto> dtoList = patientMapper.patientsListToMinimalPatientDtoList(patientsList);
 		
